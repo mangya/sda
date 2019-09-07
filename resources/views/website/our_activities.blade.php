1db7ff385f58152da648e62af3059121d317ec6f
Mangesh G <mangesh.ghadigaonkar@gmail.com>
2019-09-07 13:24:53 +0530

    new images updated

--- resources/views/website/our_activities.blade.php
+++ resources/views/website/our_activities.blade.php
@@ -32,12 +32,12 @@
       </div>
 
       <div class="row align-items-center mb-30">
-        <div class="col-12 col-lg-5">
+        <div class="col-12 col-lg-6">
           <div class="about-us-thumbnail mb-15">
-            <img src="{{asset('img/bg-img/awarness_acivity.jpg')}}" alt="">
+            <img src="{{asset('img/bg-img/seminars_collage.jpg')}}" alt="">
           </div>
         </div>
-        <div class="col-12 col-lg-7">
+        <div class="col-12 col-lg-6">
           <div class="about-us-content content">
             <h4><strong>Awareness Program</strong></h4>
             <p>A series of awareness program has been commenced by the Team since last year, which provides citizens of the City with basic tools and information on various aspects of environment, pollution, measures to control them.  The program was conducted for the different groups of people.</p>
@@ -53,7 +53,7 @@
 
       <div class="row align-items-center">
         
-        <div class="col-12 col-lg-7">
+        <div class="col-12 col-lg-6">
           <div class="about-us-content content">
             <h4><strong>Greening programme</strong></h4>
             <p>Understanding the seriousness of the problems arising out of the climate change and deforestation, the team has been trying to contribute towards the government’s National Mission for A Green India. The National Mission for A Green India or Green India Mission (GIM) is one of the eight missions India’s action plans to address the challenge of climate change. The mission recognises the influence forests on environmental amelioration through climate change mitigation, water security, food security, biodiversity conservation and livelihood security of forest living as well as dependent communities.</p>
@@ -63,9 +63,9 @@
             <p><strong>Seedball Trainings:</strong> The team conducted seed ball training workshops for the resident of the society. Seed balls are a planting method that protects the seeds until the time is right for them to grow.</p>
           </div>
         </div>
-        <div class="col-12 col-lg-5">
+        <div class="col-12 col-lg-6">
           <div class="about-us-thumbnail mb-15">
-            <img src="{{asset('img/bg-img/greening_activity.jpg')}}" alt="">
+            <img src="{{asset('img/bg-img/seedball_collage.jpg')}}" alt="">
           </div>
         </div>
       </div>
