b1b34c7261ef6855725b950e1ad3b1edef34a615
mangya <mangesh.ghadigaonkar@gmail.com>
2019-11-08 21:03:55 +0530

    page title fixed

--- resources/views/website/kdmc_downloads.blade.php
+++ resources/views/website/kdmc_downloads.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.website_layout')
-@section('title','News')
+@section('title','KDMC Gazettes')
 @section('page_content')
   <!-- ##### Breadcrumb Area Start ##### -->
   <div class="famie-breadcrumb">
@@ -13,8 +13,7 @@
     </div>
   </div>
   <!-- ##### Breadcrumb Area End ##### -->
-
-    <section class="about-us-area">
+  <section class="about-us-area">
     <div class="container">
       <div class="row align-items-center">
         <!-- About Us Content -->
@@ -25,103 +24,99 @@
               <h2><span>KDMC</span> Gazettes</h2>
               <img src="img/core-img/decor.png" alt="">
             </div>
-            
               <div class="single-widget-area">
-
-                  <div class="single-recent-blog style-2 d-flex align-items-center">
-                    <div class="post-thumbnail">
-                      <a href="storage/downloads/C&D_rules_2016.pdf"  target="_blank">
-                        <img src="website/img/core-img/pdf.png" alt="">
-                      </a>  
-                    </div>
-                    <div class="post-content">
-                      <a href="storage/downloads/C&D_rules_2016.pdf" target="_blank" class="post-title">
-                        <h5>Construction and Demolition Waste Management Rules, 2016</h5>
-                      </a>
-                        <b>The Gazette of India
-                        Title - Construction and Demolition Waste Management Rules, 2016
-                        Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
-                        </b>
-                       </br>
-                        <b>Summary -</b>
-                        The draft rules, provided a regulatory framework for Management of construction and Demolition Waste generated in the urban area of the country.
-                        The rules apply to every waste resulting from construction, remodeling, repair and demolition of any civil structure of individual or organisation or authority who generates construction and demolition waste such as building material, debris, rubble.
-                        The PDF describes duties, criterias, applications forms wth refernce to the activities covered under the Gazetted rule.</br>
-                        <a href="storage/downloads/C&D_rules_2016.pdf"  target="_blank" class="text-primary">
-                        Read More
-                      </a> 
-                      
-                    </div>
+                <div class="single-recent-blog style-2 d-flex align-items-center">
+                  <div class="post-thumbnail">
+                    <a href="storage/downloads/C&D_rules_2016.pdf"  target="_blank">
+                      <img src="website/img/core-img/pdf.png" alt="">
+                    </a>  
                   </div>
-                  <!--
-                  <div class="single-recent-blog style-2 d-flex align-items-center">
-                    <div class="post-thumbnail">
-                      <a href="storage/#"  target="_blank">
-                        <img src="website/img/core-img/pdf.png" alt="">
-                      </a>  
-                    </div>
-                    <div class="post-content">
-                      <a href="storage/#" target="_blank" class="post-title">
-                        <h5>Solid Waste Management Rules, 2016</h5>
-                      </a>  
-                        <b>The Gazette of India
-                        Title - Solid Waste Management Rules, 2016 Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
-                        </b>
-                       </br>
-                        <b>Summary -</b>
-                        The draft rules, provided a regulatory framework for Management of Municipal Solid Waste generated in the urban area of the country.</br>
-                        <a href="storage/#"  target="_blank" class="text-primary">
-                        Read More
-                      </a>
-                    </div>
-                  </div>
-                  -->
-                  <div class="single-recent-blog style-2 d-flex align-items-center">
-                    <div class="post-thumbnail">
-                      <a href="storage/downloads/PWM_Rules_2016.pdf"  target="_blank">
-                        <img src="website/img/core-img/pdf.png" alt="">
-                      </a>  
-                    </div>
-                    <div class="post-content">
-                      <a href="storage/downloads/PWM_Rules_2016.pdf" target="_blank" class="post-title">
-                        <h5>Plastic Waste Management Rules, 2016</h5>
-                      </a>  
+                  <div class="post-content">
+                    <a href="storage/downloads/C&D_rules_2016.pdf" target="_blank" class="post-title">
+                      <h5>Construction and Demolition Waste Management Rules, 2016</h5>
+                    </a>
                       <b>The Gazette of India
-                        Title - Plastic Waste Management Rules, 2016 Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
-                        </b>
-                       </br>
-                        <b>Summary -</b>
-                        The draft rules, provided a regulatory framework for Plastic waste management generated in the country. It also focuses on plastic waste minimisation, source segregation, recycling, involving waste pickers, recyclers and waste processors in collection of plastic waste fraction either from households or any other source.
-                        These rules apply to every waste generator, local body, gram Panchayat, manufacturers, Importers and producer.</br>
-                        <a href="storage/downloads/PWM_Rules_2016.pdf"  target="_blank" class="text-primary">
-                        Read More
-                      </a>
-                    </div>
+                      Title - Construction and Demolition Waste Management Rules, 2016
+                      Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
+                      </b>
+                     </br>
+                      <b>Summary -</b>
+                      The draft rules, provided a regulatory framework for Management of construction and Demolition Waste generated in the urban area of the country.
+                      The rules apply to every waste resulting from construction, remodeling, repair and demolition of any civil structure of individual or organisation or authority who generates construction and demolition waste such as building material, debris, rubble.
+                      The PDF describes duties, criterias, applications forms wth refernce to the activities covered under the Gazetted rule.</br>
+                      <a href="storage/downloads/C&D_rules_2016.pdf"  target="_blank" class="text-primary">
+                      Read More
+                    </a> 
                   </div>
-
-                  <div class="single-recent-blog style-2 d-flex align-items-center">
-                    <div class="post-thumbnail">
-                      <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf"  target="_blank">
-                        <img src="website/img/core-img/pdf.png" alt="">
-                      </a>  
-                    </div>
-                    <div class="post-content">
-                      <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf" target="_blank" class="post-title">
-                        <h5>Solid waste management and handling , cleanliness and sanitation</h5>
-                      </a>  
-                        <b>The Gazette of Maharashtra Government
-                        Title - Solid waste management and handling , cleanliness and sanitation Published in the Gazette of Maharashtra, part-IV B
-                        </b>
-                       </br>
-                        <b>Summary -</b>
-                        The draft rules, provided a regulatory framework for Solid waste (management and handling), cleanliness and sanitation by laws for Maharashtra Municipal Corporation.
-                        These by-laws are applicable within the limits of all the Municipal Corporations of the Maharashtra and each and every public place within the jurisdiction of Municipal Corporations, to every generator of municipal solid waste and to every premise under the ownership or occupation of any person/ institute within the limits of the Municipal Corporations.</br>
-                        <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf"  target="_blank" class="text-primary">
-                        Read More
-                      </a>
-                    </div>
+                </div>
+                <!--
+                <div class="single-recent-blog style-2 d-flex align-items-center">
+                  <div class="post-thumbnail">
+                    <a href="storage/#"  target="_blank">
+                      <img src="website/img/core-img/pdf.png" alt="">
+                    </a>  
                   </div>
- 
+                  <div class="post-content">
+                    <a href="storage/#" target="_blank" class="post-title">
+                      <h5>Solid Waste Management Rules, 2016</h5>
+                    </a>  
+                      <b>The Gazette of India
+                      Title - Solid Waste Management Rules, 2016 Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
+                      </b>
+                     </br>
+                      <b>Summary -</b>
+                      The draft rules, provided a regulatory framework for Management of Municipal Solid Waste generated in the urban area of the country.</br>
+                      <a href="storage/#"  target="_blank" class="text-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+                -->
+                <div class="single-recent-blog style-2 d-flex align-items-center">
+                  <div class="post-thumbnail">
+                    <a href="storage/downloads/PWM_Rules_2016.pdf"  target="_blank">
+                      <img src="website/img/core-img/pdf.png" alt="">
+                    </a>  
+                  </div>
+                  <div class="post-content">
+                    <a href="storage/downloads/PWM_Rules_2016.pdf" target="_blank" class="post-title">
+                      <h5>Plastic Waste Management Rules, 2016</h5>
+                    </a>  
+                    <b>The Gazette of India
+                      Title - Plastic Waste Management Rules, 2016 Published in the Gazette of India, part-II, Section-3, Sub-section (ii)
+                      </b>
+                     </br>
+                      <b>Summary -</b>
+                      The draft rules, provided a regulatory framework for Plastic waste management generated in the country. It also focuses on plastic waste minimisation, source segregation, recycling, involving waste pickers, recyclers and waste processors in collection of plastic waste fraction either from households or any other source.
+                      These rules apply to every waste generator, local body, gram Panchayat, manufacturers, Importers and producer.</br>
+                      <a href="storage/downloads/PWM_Rules_2016.pdf"  target="_blank" class="text-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+                <div class="single-recent-blog style-2 d-flex align-items-center">
+                  <div class="post-thumbnail">
+                    <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf"  target="_blank">
+                      <img src="website/img/core-img/pdf.png" alt="">
+                    </a>  
+                  </div>
+                  <div class="post-content">
+                    <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf" target="_blank" class="post-title">
+                      <h5>Solid waste management and handling , cleanliness and sanitation</h5>
+                    </a>  
+                      <b>The Gazette of Maharashtra Government
+                      Title - Solid waste management and handling , cleanliness and sanitation Published in the Gazette of Maharashtra, part-IV B
+                      </b>
+                     </br>
+                      <b>Summary -</b>
+                      The draft rules, provided a regulatory framework for Solid waste (management and handling), cleanliness and sanitation by laws for Maharashtra Municipal Corporation.
+                      These by-laws are applicable within the limits of all the Municipal Corporations of the Maharashtra and each and every public place within the jurisdiction of Municipal Corporations, to every generator of municipal solid waste and to every premise under the ownership or occupation of any person/ institute within the limits of the Municipal Corporations.</br>
+                      <a href="storage/downloads/Final_by_laws_corporation_July_2019.pdf"  target="_blank" class="text-primary">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
           </div>
         </div>
       </div>
